e9a9a75eefd53dbea805b20a8aca6b604a93c95f
ваше имя <zabenkotoi@gmail.com>
2020-11-02 14:25:57 +0300

    almost done

---
+++ OSHandle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BinarySemaphore
+{
+    internal class OSHandle : IDisposable
+    {
+        private bool _disposed = false;
+
+        public IntPtr Handle { get; set; }
+
+        private _Mutex myMutex = new _Mutex();
+
+        public OSHandle(IntPtr ptr)
+        {
+            Handle = ptr;
+        }
+
+        ~OSHandle() => Dispose(false);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            myMutex.Lock();
+            if (_disposed)
+            {
+                return;
+            }
+            if (disposing && Handle != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(Handle);
+                Handle = IntPtr.Zero;
+            }            
+            _disposed = true;
+            myMutex.UnLock();
+        }       
+    }
+}
---
+++ _Mutex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BinarySemaphore
+{
+    class _Mutex
+    {
+        private int IsLocked = 0;
+
+        public void Lock()
+        {
+            while (Interlocked.CompareExchange(ref IsLocked, 1, 0) == 1)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        public void UnLock()
+        {
+            Interlocked.Exchange(ref IsLocked, 0);
+        }
+    }
+}
